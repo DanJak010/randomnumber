396a2be771204e8f194b226b4750f4fa7e50a2e7
DanJak010 <daniele.martini2705@gmail.com>
2022-02-06 23:52:55 +0100

    Init

---
+++ app.js
@@ -0,0 +1,21 @@
+const generateBtn = document.querySelector(".generateBtn");
+
+generateBtn.addEventListener("click", generateRandomNum);
+
+function generateRandomNum() {
+  const fromNum = document.querySelector(".fromNum").value;
+  const toNum = document.querySelector(".toNum").value;
+
+  randomNum = Math.floor(Math.random() * parseInt(toNum));
+
+  while ((randomNum == 4) || (randomNum == 13) || (randomNum == 15)){
+    randomNum = Math.floor(Math.random() * parseInt(toNum))
+  }
+
+  if (randomNum >= parseInt(fromNum)) {
+    const randomNumEl = document.querySelector(".randomNumEl");
+    randomNumEl.innerHTML = randomNum;
+  }
+
+}
+
